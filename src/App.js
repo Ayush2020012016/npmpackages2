ff49aef1936b93110733fe57bd65bc80d6a7a124
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-05-11 22:03:39 +0530

    app.js added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Homepage from "./pages/Homepage.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddFavorite from "./pages/AddFavorite.js";
+import View from "./pages/View.js";
+import Edit from "./pages/Edit.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/add" element={<AddFavorite />} />
+        <Route exact path="/edit" element={<Edit />} />
+        <Route exact path="/view" element={<View />} />
+        <Route exact path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
