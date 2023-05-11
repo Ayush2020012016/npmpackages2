ba75c47bf9526dc8f62674bd5c01c729a41dc2cc
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-05-11 22:30:26 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Homepage from "./pages/Homepage.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddFavorite from "./pages/AddFavorite.js";
+import View from "./pages/View.js";
+import Edit from "./pages/Edit.js";
 
 const App = () => {
   return <div>ayush</div>;
